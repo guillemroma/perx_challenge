764f891d3d2e1a0a21130448b1bcf8f2edb6f317
Guillem Roma <guillemroma.lopez@gmail.com>
2022-10-03 12:49:56 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show, :index]
+  namespace :dashboard, only:[] do
+    resources :users, only: [:show]
+  end
+  namespace :transactions, only: [] do
+    resources :users, only: [:show]
+  end
 end
